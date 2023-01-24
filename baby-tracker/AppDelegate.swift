d3c8c4b7d6bb9ddd464e8c0824d82b3a87d29f46
Eliany Barrera <50035462+ebarrera95@users.noreply.github.com>
2023-01-24 16:12:38 +1100

    Initial Commit

---
+++ baby-tracker/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  baby-tracker
+//
+//  Created by Eliany Barrera on 24/1/2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ baby-tracker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  baby-tracker
+//
+//  Created by Eliany Barrera on 24/1/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
